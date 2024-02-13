79410b977cd53ce215439d4df7f5c00b8645af77
solarnius <solarnius@sagods.com>
2024-02-13 15:59:34 -0800

    latest

--- python/server.py
+++ python/server.py
@@ -120,6 +120,7 @@ def classify_one(id):
   imageUrl = ""
   if "cdn_uri" in rpcResponse["result"]["content"]["files"][0]:
     imageUrl = rpcResponse["result"]["content"]["files"][0]["cdn_uri"]
+    imageUrl =rpcResponse["result"]["content"]["links"]["image"]
   else:
     imageUrl =rpcResponse["result"]["content"]["links"]["image"]
 
